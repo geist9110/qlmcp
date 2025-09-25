7be0e102120eb942f33bf27772431fb0dff93665
geist9110 <geist9110@naver.com>
2025-09-25 16:46:20 +0900

    feat: 시스템 프롬프트 추가

--- backend/src/main/java/com/qlmcp/backend/config/ChatConfig.java
+++ backend/src/main/java/com/qlmcp/backend/config/ChatConfig.java
@@ -1,8 +1,10 @@
 package com.qlmcp.backend.config;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.modelcontextprotocol.client.McpSyncClient;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.Setter;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.mcp.SyncMcpToolCallbackProvider;
 import org.springframework.ai.tool.ToolCallback;
@@ -12,13 +14,19 @@ import org.springframework.ai.tool.resolution.SpringBeanToolCallbackResolver;
 import org.springframework.ai.tool.resolution.StaticToolCallbackResolver;
 import org.springframework.ai.tool.resolution.ToolCallbackResolver;
 import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.support.GenericApplicationContext;
 
+@Setter
 @Configuration
+@ConfigurationProperties(prefix = "spring.ai.openai.chat.options")
 public class ChatConfig {
+
+    @JsonProperty("system-prompt")
+    private String systemPrompt;
 
     /**
      * ToolCallbackResolver 빈 오버라이딩으로 기본적인 토대는 동일하다. LLM이 외부 MCP 툴을 콜백으로 받도록 수정하여 내부에서 정의된 툴을 사용할 수
@@ -58,6 +66,7 @@ public class ChatConfig {
         ObjectProvider<List<McpSyncClient>> syncMcpClients
     ) {
         return chatClientBuilder
+            .defaultSystem(this.systemPrompt)
             .defaultToolCallbacks(getSyncMcpToolCallbackProvider(syncMcpClients))
             .build();
     }
