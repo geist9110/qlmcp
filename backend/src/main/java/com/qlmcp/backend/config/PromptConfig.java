07385315f2b998bb186d009675d22b7785fe217b
geist9110 <geist9110@naver.com>
2025-09-28 12:49:40 +0900

    refactor: 필요없는 JsonProperty 삭제

--- backend/src/main/java/com/qlmcp/backend/config/PromptConfig.java
+++ backend/src/main/java/com/qlmcp/backend/config/PromptConfig.java
@@ -1,6 +1,5 @@
 package com.qlmcp.backend.config;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -12,15 +11,11 @@ import org.springframework.context.annotation.Configuration;
 @ConfigurationProperties(prefix = "spring.ai.openai.chat.options.prompt")
 public class PromptConfig {
 
-    @JsonProperty("system-prompt")
     private String systemPrompt;
 
-    @JsonProperty("user-prompt-start")
     private String userPromptStart;
 
-    @JsonProperty("user-prompt-end")
     private String userPromptEnd;
 
-    @JsonProperty("log-tag")
     private String logTag;
 }
