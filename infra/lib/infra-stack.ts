10080405f405d921d7fd43061ef8be102ceb276b
geist9110 <geist9110@naver.com>
2025-09-29 17:36:19 +0900

    feat: vpc 추가

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -3,14 +3,13 @@ import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import {Construct} from 'constructs';
 
 export class InfraStack extends cdk.Stack {
-  public readonly instance: ec2.Instance;
   public readonly vpc: ec2.Vpc;
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     this.vpc = new ec2.Vpc(this, 'qlmcp-vpc', {
-      maxAzs: 2,
+      maxAzs: 1,
       natGateways: 0,
       subnetConfiguration: [
         {
@@ -19,63 +18,6 @@ export class InfraStack extends cdk.Stack {
           cidrMask: 24,
         },
       ],
-    });
-
-    const securityGroup = new ec2.SecurityGroup(this, 'InstanceSecurityGroup', {
-      vpc: this.vpc,
-      description: "Allow SSH and HTTP access",
-      allowAllOutbound: true,
-    });
-
-    securityGroup.addIngressRule(
-        ec2.Peer.anyIpv4(),
-        ec2.Port.tcp(22),
-        "Allow SSH access from anywhere"
-    );
-
-    securityGroup.addIngressRule(
-        ec2.Peer.anyIpv4(),
-        ec2.Port.tcp(80),
-        "Allow HTTP access from anywhere"
-    );
-
-    this.instance = new ec2.Instance(this, "QLMCP Main server", {
-      vpc: this.vpc,
-      instanceType: ec2.InstanceType.of(
-          ec2.InstanceClass.T3,
-          ec2.InstanceSize.MICRO
-      ),
-      machineImage: ec2.MachineImage.latestAmazonLinux2023(),
-      securityGroup: securityGroup,
-      vpcSubnets: {
-        subnetType: ec2.SubnetType.PUBLIC,
-      },
-      userData: ec2.UserData.forLinux()
-    });
-
-    // User Data에 간단한 웹서버 설치 스크립트 추가
-    this.instance.userData.addCommands(
-        'yum update -y',
-        'yum install -y httpd',
-        'systemctl start httpd',
-        'systemctl enable httpd',
-        'echo "<h1>Hello from CDK!</h1>" > /var/www/html/index.html'
-    );
-
-    // 출력값 정의
-    new cdk.CfnOutput(this, 'InstanceId', {
-      value: this.instance.instanceId,
-      description: 'EC2 Instance ID',
-    });
-
-    new cdk.CfnOutput(this, 'InstancePublicIp', {
-      value: this.instance.instancePublicIp,
-      description: 'EC2 Instance Public IP',
-    });
-
-    new cdk.CfnOutput(this, 'InstancePublicDnsName', {
-      value: this.instance.instancePublicDnsName,
-      description: 'EC2 Instance Public DNS Name',
     });
   }
 }
--- infra/test/infra.test.ts
+++ infra/test/infra.test.ts
@@ -13,83 +13,43 @@ describe('InfraStack', () => {
     template = Template.fromStack(stack);
   });
 
-  // VPC 생성 확인
-  test('Creates a VPC', () => {
-    template.resourceCountIs('AWS::EC2::VPC', 1);
-  });
+  describe('VPC 테스트', () => {
+    test('하나의 VPC만 생성되는지 확인', () => {
+      template.resourceCountIs('AWS::EC2::VPC', 1);
+    })
 
-  // EC2 인스턴스 생성 확인
-  test('Creates an EC2 Instance', () => {
-    template.resourceCountIs('AWS::EC2::Instance', 1);
-  });
+    test("VPC가 /16 CIDR 블록을 가지는지 확인", () => {
+      template.hasResourceProperties('AWS::EC2::VPC', {
+        CidrBlock: Match.stringLikeRegexp('^10\\.0\\.0\\.0/16$')
+      })
+    })
 
-  // EC2 인스턴스 속성 확인
-  test('EC2 Instance has correct properties', () => {
-    template.hasResourceProperties('AWS::EC2::Instance', {
-      InstanceType: 't3.micro',
-    });
-  });
+    test("VPC에 NatGateway가 없는지 확인", () => {
+      template.resourceCountIs('AWS::EC2::NatGateway', 0);
+    })
 
-  // Security Group 생성 확인
-  test('Creates a Security Group', () => {
-    template.resourceCountIs('AWS::EC2::SecurityGroup', 1);
-  });
+    test("VPC가 하나의 Public Subnet을 가지는지 확인", () => {
+      template.resourceCountIs('AWS::EC2::Subnet', 1);
+    })
 
-  // Security Group에 SSH 규칙이 있는지 확인
-  test('Security Group allows SSH access', () => {
-    template.hasResourceProperties('AWS::EC2::SecurityGroup', {
-      SecurityGroupIngress: Match.arrayWith([
-        Match.objectLike({
-          IpProtocol: 'tcp',
-          FromPort: 22,
-          ToPort: 22,
-          CidrIp: '0.0.0.0/0',
-        }),
-      ]),
-    });
-  });
+    test("Subnet의 CIDR 마스크가 /24인지 확인", () => {
+      template.hasResourceProperties('AWS::EC2::Subnet', {
+        MapPublicIpOnLaunch: true,
+        CidrBlock: Match.stringLikeRegexp('^10\\.0\\.0\\.0/24$'),
+      });
+    })
 
-  // Security Group에 HTTP 규칙이 있는지 확인
-  test('Security Group allows HTTP access', () => {
-    template.hasResourceProperties('AWS::EC2::SecurityGroup', {
-      SecurityGroupIngress: Match.arrayWith([
-        Match.objectLike({
-          IpProtocol: 'tcp',
-          FromPort: 80,
-          ToPort: 80,
-          CidrIp: '0.0.0.0/0',
-        }),
-      ]),
-    });
-  });
+    test("Internet Gateway가 생성되는지 확인", () => {
+      template.resourceCountIs('AWS::EC2::InternetGateway', 1);
+    })
 
-  // User Data가 설정되어 있는지 확인
-  test('EC2 Instance has UserData configured', () => {
-    template.hasResourceProperties('AWS::EC2::Instance', {
-      UserData: Match.anyValue(),
-    });
-  });
-
-  // 출력값 확인
-  test('Stack has required outputs', () => {
-    template.hasOutput('InstanceId', {});
-    template.hasOutput('InstancePublicIp', {});
-    template.hasOutput('InstancePublicDnsName', {});
-  });
-
-  // Subnet이 Public인지 확인
-  test('Instance is in a public subnet', () => {
-    template.hasResourceProperties('AWS::EC2::Instance', {
-      SubnetId: Match.anyValue(),
-    });
-  });
-
-  // 리소스 카운트 전체 검증
-  test('Stack has expected number of resources', () => {
-    const resources = template.toJSON().Resources;
-    const resourceCount = Object.keys(resources).length;
-
-    // VPC, Subnet, RouteTable, IGW, SecurityGroup, Instance 등
-    expect(resourceCount).toBeGreaterThan(5);
-  });
+    test("VPC가 Internet Gateway에 연결되는지 확인", () => {
+      template.hasResourceProperties('AWS::EC2::Route',
+          Match.objectLike({
+            DestinationCidrBlock: '0.0.0.0/0',
+            GatewayId: Match.anyValue(),
+          })
+      );
+    })
+  })
 });
