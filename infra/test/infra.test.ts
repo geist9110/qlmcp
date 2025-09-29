5885d78ed32242f1c4a038a2fa2fc6d2d78230fc
geist9110 <geist9110@naver.com>
2025-09-29 16:03:48 +0900

    feat: 인스턴스 생성 및 테스트 예제 추가

--- infra/test/infra.test.ts
+++ infra/test/infra.test.ts
@@ -1,17 +1,95 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Infra from '../lib/infra-stack';
+import * as cdk from 'aws-cdk-lib';
+import {Match, Template} from 'aws-cdk-lib/assertions';
+import {InfraStack} from '../lib/infra-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/infra-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Infra.InfraStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+describe('InfraStack', () => {
+  let app: cdk.App;
+  let stack: InfraStack;
+  let template: Template;
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  beforeEach(() => {
+    app = new cdk.App();
+    stack = new InfraStack(app, 'TestStack');
+    template = Template.fromStack(stack);
+  });
+
+  // VPC 생성 확인
+  test('Creates a VPC', () => {
+    template.resourceCountIs('AWS::EC2::VPC', 1);
+  });
+
+  // EC2 인스턴스 생성 확인
+  test('Creates an EC2 Instance', () => {
+    template.resourceCountIs('AWS::EC2::Instance', 1);
+  });
+
+  // EC2 인스턴스 속성 확인
+  test('EC2 Instance has correct properties', () => {
+    template.hasResourceProperties('AWS::EC2::Instance', {
+      InstanceType: 't3.micro',
+    });
+  });
+
+  // Security Group 생성 확인
+  test('Creates a Security Group', () => {
+    template.resourceCountIs('AWS::EC2::SecurityGroup', 1);
+  });
+
+  // Security Group에 SSH 규칙이 있는지 확인
+  test('Security Group allows SSH access', () => {
+    template.hasResourceProperties('AWS::EC2::SecurityGroup', {
+      SecurityGroupIngress: Match.arrayWith([
+        Match.objectLike({
+          IpProtocol: 'tcp',
+          FromPort: 22,
+          ToPort: 22,
+          CidrIp: '0.0.0.0/0',
+        }),
+      ]),
+    });
+  });
+
+  // Security Group에 HTTP 규칙이 있는지 확인
+  test('Security Group allows HTTP access', () => {
+    template.hasResourceProperties('AWS::EC2::SecurityGroup', {
+      SecurityGroupIngress: Match.arrayWith([
+        Match.objectLike({
+          IpProtocol: 'tcp',
+          FromPort: 80,
+          ToPort: 80,
+          CidrIp: '0.0.0.0/0',
+        }),
+      ]),
+    });
+  });
+
+  // User Data가 설정되어 있는지 확인
+  test('EC2 Instance has UserData configured', () => {
+    template.hasResourceProperties('AWS::EC2::Instance', {
+      UserData: Match.anyValue(),
+    });
+  });
+
+  // 출력값 확인
+  test('Stack has required outputs', () => {
+    template.hasOutput('InstanceId', {});
+    template.hasOutput('InstancePublicIp', {});
+    template.hasOutput('InstancePublicDnsName', {});
+  });
+
+  // Subnet이 Public인지 확인
+  test('Instance is in a public subnet', () => {
+    template.hasResourceProperties('AWS::EC2::Instance', {
+      SubnetId: Match.anyValue(),
+    });
+  });
+
+  // 리소스 카운트 전체 검증
+  test('Stack has expected number of resources', () => {
+    const resources = template.toJSON().Resources;
+    const resourceCount = Object.keys(resources).length;
+
+    // VPC, Subnet, RouteTable, IGW, SecurityGroup, Instance 등
+    expect(resourceCount).toBeGreaterThan(5);
+  });
 });
