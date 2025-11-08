69045408cbab23caba7622029559b0552aadf1cc
Lee Hyungi <geist9110@naver.com>
2025-11-08 17:16:04 +0900

    test: oauth-authorization-server 테스트 추가

--- backend/src/test/java/com/qlmcp/backend/controller/WellknownControllerTest.java
+++ backend/src/test/java/com/qlmcp/backend/controller/WellknownControllerTest.java
@@ -20,7 +20,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -57,7 +56,6 @@ public class WellknownControllerTest {
     }
 
     @Test
-    @WithAnonymousUser
     @DisplayName("Get OAuth Protected Resource Metadata")
     void getOAuthProtectedResourceMetadata() throws Exception {
         //then
@@ -69,6 +67,30 @@ public class WellknownControllerTest {
                     objectMapper.writeValueAsString(
                         Map.of("resource", "http://localhost:8080/mcp",
                             "authorization_servers", List.of("http://localhost:8080"))
+                    )
+                )
+            );
+    }
+
+    @Test
+    @DisplayName("Get Authorization Server Metadata")
+    void getAuthorizationServerMetadata() throws Exception {
+        mockMvc.perform(get("/.well-known/oauth-authorization-server"))
+            .andExpectAll(
+                status().isOk(),
+                content().contentType(MediaType.APPLICATION_JSON),
+                content().json(
+                    objectMapper.writeValueAsString(
+                        Map.of(
+                            "issuer", "http://localhost:8080",
+                            "registration_endpoint", "http://localhost:8080/dcr/register",
+                            "authorization_endpoint", "http://localhost:8080/oauth2/authorize",
+                            "response_types_supported", List.of("code"),
+                            "grant_types_supported", List.of("authorization_code", "refresh_token"),
+                            "token_endpoint_auth_methods_supported",
+                            List.of("client_secret_basic", "client_secret_post"),
+                            "code_challenge_methods_supported", List.of("S256"),
+                            "scopes_supported", List.of("openid"))
                     )
                 )
             );
