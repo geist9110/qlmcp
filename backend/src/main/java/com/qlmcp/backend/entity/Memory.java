422b385f517b759d106c05b1a5aa0b77c966fc39
Lee Hyungi <geist9110@naver.com>
2025-11-10 08:53:08 +0900

    feat: Memory Entity 추가

---
+++ backend/src/main/java/com/qlmcp/backend/entity/Memory.java
@@ -0,0 +1,46 @@
+package com.qlmcp.backend.entity;
+
+import java.time.LocalDateTime;
+
+import com.qlmcp.backend.dto.MemoryCategory;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@EnableJpaAuditing
+public class Memory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    private Account account;
+
+    @Enumerated(EnumType.STRING)
+    private MemoryCategory memoryCategory;
+
+    private String content;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
