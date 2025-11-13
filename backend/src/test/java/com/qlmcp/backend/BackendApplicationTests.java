264427b0fc78bd45170968c1056f9efbb1095b3d
Lee Hyungi <geist9110@naver.com>
2025-11-13 16:58:24 +0900

    refactor: server의 tool-callback-converter를 false로 변경
    기존에는 Client의 Tool이 ToolCallbackProvider를 생성하지 안도록 구성되어 있었지만,
    ToolCallbackProvider의 Tool을 SyncToolSpecification으로 변환해주는 설정을 false로
    변경하여 @McpTool 어노테이션에 한해서 외부로 노출되는 Tool을 사용할 수 있도록 변경

--- backend/src/test/java/com/qlmcp/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/qlmcp/backend/BackendApplicationTests.java
@@ -1,26 +1,16 @@
 package com.qlmcp.backend;
 
-import static com.qlmcp.backend.util.ReflectionHelper.getFieldValue;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.List;
-import java.util.Map;
-
-import com.qlmcp.backend.tool.MemoryTool;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.mcp.SyncMcpToolCallbackProvider;
-import org.springframework.ai.tool.ToolCallbackProvider;
-import org.springframework.ai.tool.method.MethodToolCallbackProvider;
-import org.springframework.ai.tool.resolution.ToolCallbackResolver;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
 import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -38,19 +28,7 @@ class BackendApplicationTests {
     ObjectProvider<List<McpSyncClient>> syncMcpClients;
 
     @Autowired
-    private ToolCallbackResolver toolCallbackResolver;
-
-    @Autowired
-    private ChatClient chatClient;
-
-    @Autowired
-    private ApplicationContext applicationContext;
-
-    @Autowired
     private Environment environment;
-
-    @Autowired
-    private MemoryTool memoryTool;
 
     @Test
     @DisplayName("Spring Application Context가 정상적으로 로드되는지 테스트")
@@ -64,48 +42,5 @@ class BackendApplicationTests {
     void internalToolTest() {
         List<SyncToolSpecification> serverTools = tools.stream().flatMap(List::stream).toList();
         assertAll(() -> assertNotNull(serverTools), () -> assertEquals(1, serverTools.size()));
-    }
-
-    @Test
-    @DisplayName("ToolCallbackResolver가 외부 툴만 불러오는지 테스트")
-    void externalToolTest() {
-        List<?> actualResolver = getFieldValue(toolCallbackResolver, "toolCallbackResolvers", List.class);
-        Map<?, ?> actualToolCallbackMap = getFieldValue(actualResolver.getFirst(), "toolCallbacks", Map.class);
-
-        assertAll(
-                () -> assertNotNull(toolCallbackResolver),
-                () -> assertEquals(
-                        getSyncToolCallbackProvider().getToolCallbacks().length,
-                        actualToolCallbackMap.size()));
-    }
-
-    private SyncMcpToolCallbackProvider getSyncToolCallbackProvider() {
-        List<McpSyncClient> mcpClients = syncMcpClients.stream().flatMap(List::stream).toList();
-        return SyncMcpToolCallbackProvider.builder().mcpClients(mcpClients).build();
-    }
-
-    @Test
-    @DisplayName("ChatClient가 외부 툴을 기본으로 가지는지 테스트")
-    void chatClientTest() {
-        Object defaultChatClientRequest = getFieldValue(chatClient, "defaultChatClientRequest", Object.class);
-
-        List<?> actualToolCallbacks = getFieldValue(defaultChatClientRequest, "toolCallbacks", List.class);
-
-        ToolCallbackProvider memoryToolCallbackProvider = MethodToolCallbackProvider.builder().toolObjects(memoryTool)
-                .build();
-
-        assertEquals(
-                getSyncToolCallbackProvider().getToolCallbacks().length
-                        + memoryToolCallbackProvider.getToolCallbacks().length,
-                actualToolCallbacks.size());
-    }
-
-    @Test
-    @DisplayName("Application Context가 SyncMcpToolCallbackProvider을 반환값으로 가지는 빈을 가지지 않는지 확인하는 테스트")
-    void noSyncMcpToolCallbackProviderBeanTest() {
-        Map<String, SyncMcpToolCallbackProvider> beans = applicationContext
-                .getBeansOfType(SyncMcpToolCallbackProvider.class);
-
-        assertEquals(0, beans.size());
     }
 }
