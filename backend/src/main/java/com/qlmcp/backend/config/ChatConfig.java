e8d8d6e005c85665073c2db18cd3d52dc9436f0f
Lee Hyungi <63250720+geist9110@users.noreply.github.com>
2025-11-17 11:07:57 +0900

    Tool 설정 리팩토링 (#23)
    * refactor: server의 tool-callback-converter를 false로 변경
    
    기존에는 Client의 Tool이 ToolCallbackProvider를 생성하지 안도록 구성되어 있었지만,
    ToolCallbackProvider의 Tool을 SyncToolSpecification으로 변환해주는 설정을 false로
    변경하여 @McpTool 어노테이션에 한해서 외부로 노출되는 Tool을 사용할 수 있도록 변경
    
    * chore: tool-callback-converter 속성 warning 처리
    
    * chore: spring-ai 1.1.0-M3에서 1.1.0 stable버전으로 변경
    
    * style: 미사용 어노테이션 제거
    
    * refactor: 미사용 ReflectionHelper 클래스 제거

--- backend/src/main/java/com/qlmcp/backend/config/ChatConfig.java
+++ backend/src/main/java/com/qlmcp/backend/config/ChatConfig.java
@@ -1,82 +1,31 @@
 package com.qlmcp.backend.config;
 
-import com.qlmcp.backend.tool.MemoryTool;
-
-import io.modelcontextprotocol.client.McpSyncClient;
+import java.util.Collection;
+import java.util.List;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.mcp.SyncMcpToolCallbackProvider;
 import org.springframework.ai.tool.ToolCallback;
 import org.springframework.ai.tool.ToolCallbackProvider;
-import org.springframework.ai.tool.method.MethodToolCallbackProvider;
-import org.springframework.ai.tool.resolution.DelegatingToolCallbackResolver;
-import org.springframework.ai.tool.resolution.SpringBeanToolCallbackResolver;
-import org.springframework.ai.tool.resolution.StaticToolCallbackResolver;
-import org.springframework.ai.tool.resolution.ToolCallbackResolver;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.support.GenericApplicationContext;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 public class ChatConfig {
 
-    /**
-     * ToolCallbackResolver 빈 오버라이딩으로 기본적인 토대는 동일하다. LLM이 외부 MCP 툴을 콜백으로 받도록 수정하여 내부에서 정의된 툴을 사용할 수
-     * 없도록한다.
-     *
-     * @see : ToolCallingAutoConfiguration#toolCallbackResolver
-     */
-    @Bean
-    @Primary
-    public ToolCallbackResolver externalToolCallbackResolver(
-            GenericApplicationContext applicationContext,
-            List<ToolCallback> toolCallbacks,
-            ObjectProvider<List<McpSyncClient>> syncMcpClients) {
-        List<ToolCallbackProvider> tcbProviders =
-                List.of(getSyncMcpToolCallbackProvider(syncMcpClients));
-
-        List<ToolCallback> allFunctionAndToolCallbacks = new ArrayList<>(toolCallbacks);
-        tcbProviders.stream()
-                .map(pr -> List.of(pr.getToolCallbacks()))
-                .forEach(allFunctionAndToolCallbacks::addAll);
-
-        var staticToolCallbackResolver =
-                new StaticToolCallbackResolver(allFunctionAndToolCallbacks);
-
-        var springBeanToolCallbackResolver =
-                SpringBeanToolCallbackResolver.builder()
-                        .applicationContext(applicationContext)
-                        .build();
-
-        return new DelegatingToolCallbackResolver(
-                List.of(staticToolCallbackResolver, springBeanToolCallbackResolver));
-    }
-
     @Bean
     public ChatClient chatClient(
             ChatClient.Builder chatClientBuilder,
-            ObjectProvider<List<McpSyncClient>> syncMcpClients,
             PromptConfig promptConfig,
-            MemoryTool memoryTool) {
-        ToolCallbackProvider memoryToolCallbackProvider =
-                MethodToolCallbackProvider.builder().toolObjects(memoryTool).build();
+            List<ToolCallbackProvider> toolCallbackProviders) {
+        List<ToolCallback> providerToolCallbacks = toolCallbackProviders
+                .stream()
+                .map(pr -> List.of(pr.getToolCallbacks()))
+                .flatMap(Collection::stream)
+                .toList();
 
         return chatClientBuilder
                 .defaultSystem(promptConfig.getSystemPrompt())
-                .defaultToolCallbacks(
-                        getSyncMcpToolCallbackProvider(syncMcpClients), memoryToolCallbackProvider)
+                .defaultToolCallbacks(providerToolCallbacks)
                 .build();
-    }
-
-    // Bean으로 등록시 외부에 Tool이 노출되므로 등록하면 안된다.
-    private SyncMcpToolCallbackProvider getSyncMcpToolCallbackProvider(
-            ObjectProvider<List<McpSyncClient>> syncMcpClients) {
-        List<McpSyncClient> mcpClients = syncMcpClients.stream().flatMap(List::stream).toList();
-        return SyncMcpToolCallbackProvider.builder().mcpClients(mcpClients).build();
     }
 }
