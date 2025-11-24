12139e7d6c118a5256ceb9bba50384515c2a1752
Lee Hyungi <geist9110@naver.com>
2025-11-24 19:22:30 +0900

    chore: 불필요한 로그처리 삭제

--- backend/src/main/java/com/qlmcp/backend/controller/DcrController.java
+++ backend/src/main/java/com/qlmcp/backend/controller/DcrController.java
@@ -1,15 +1,14 @@
 package com.qlmcp.backend.controller;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
 
 import com.qlmcp.backend.dto.ClientRegistrationRequest;
 import com.qlmcp.backend.dto.ClientRegistrationResponse;
 import com.qlmcp.backend.entity.Client;
 import com.qlmcp.backend.repository.ClientRepository;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
@@ -18,7 +17,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/dcr")
@@ -28,47 +28,32 @@ public class DcrController {
 
     @PostMapping("/register")
     public ResponseEntity<ClientRegistrationResponse> registerClient(
-        @RequestBody ClientRegistrationRequest request
-    ) {
-
-        log.info("=== Dynamic Client Registration Request START ===");
-        log.info("Client Name: {}", request.clientName());
-        log.info("Redirect URIs: {}", request.redirectUris());
-
+            @RequestBody ClientRegistrationRequest request) {
         Client client = clientRepository
-            .findByClientName(request.clientName())
-            .orElseGet(
-                () -> {
-                    Client newClient = Client.builder()
-                        .id(UUID.randomUUID().toString())
-                        .clientId(UUID.randomUUID().toString())
-                        .clientSecret(UUID.randomUUID().toString())
-                        .clientName(request.clientName())
-                        .redirectUris(new HashSet<>(request.redirectUris()))
-                        .authorizationGrantTypes(Set.of(
-                                AuthorizationGrantType.AUTHORIZATION_CODE,
-                                AuthorizationGrantType.REFRESH_TOKEN
-                            )
-                        )
-                        .authenticationMethods(
-                            Set.of(ClientAuthenticationMethod.CLIENT_SECRET_BASIC))
-                        .scopes(Set.of("openid"))
-                        .build();
+                .findByClientName(request.clientName())
+                .orElseGet(
+                        () -> {
+                            Client newClient = Client.builder()
+                                    .id(UUID.randomUUID().toString())
+                                    .clientId(UUID.randomUUID().toString())
+                                    .clientSecret(UUID.randomUUID().toString())
+                                    .clientName(request.clientName())
+                                    .redirectUris(new HashSet<>(request.redirectUris()))
+                                    .authorizationGrantTypes(Set.of(
+                                            AuthorizationGrantType.AUTHORIZATION_CODE,
+                                            AuthorizationGrantType.REFRESH_TOKEN))
+                                    .authenticationMethods(
+                                            Set.of(ClientAuthenticationMethod.CLIENT_SECRET_BASIC))
+                                    .scopes(Set.of("openid"))
+                                    .build();
 
-                    return clientRepository.save(newClient);
-                }
-            );
-
-        log.info("Client Id: {}", client.getClientId());
-        log.info("Client Secret: {}", client.getClientSecret());
-        log.info("=== Dynamic Client Registration Request END ===");
+                            return clientRepository.save(newClient);
+                        });
 
         return ResponseEntity.ok(
-            new ClientRegistrationResponse(
-                client.getClientId(),
-                client.getClientSecret(),
-                request.redirectUris()
-            )
-        );
+                new ClientRegistrationResponse(
+                        client.getClientId(),
+                        client.getClientSecret(),
+                        request.redirectUris()));
     }
 }
