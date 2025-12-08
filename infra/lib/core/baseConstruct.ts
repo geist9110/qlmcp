5b6962f536289d249f4ceeb177c5ebc195936032
Lee Hyungi <geist9110@naver.com>
2025-12-08 17:21:28 +0900

    feat: baseConstruct 추가

---
+++ infra/lib/core/baseConstruct.ts
@@ -0,0 +1,27 @@
+import { Tags } from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+export interface BaseConstructProps {
+  project: string;
+  envName: string;
+}
+
+export class BaseConstruct extends Construct {
+  protected readonly project: string;
+  protected readonly environment: string;
+
+  constructor(scope: Construct, id: string, props: BaseConstructProps) {
+    super(scope, id);
+
+    this.project = props.project;
+    this.environment = props.envName;
+
+    this.addTags(id);
+  }
+
+  private addTags(id: string) {
+    Tags.of(this).add("Project", this.project);
+    Tags.of(this).add("Environment", this.environment);
+    Tags.of(this).add("Name", id);
+  }
+}
