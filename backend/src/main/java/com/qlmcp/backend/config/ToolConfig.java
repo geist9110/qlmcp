1e9ed40829d6714496c42d312e388be329757acd
Lee Hyungi <geist9110@naver.com>
2025-12-08 14:25:27 +0900

    refactor: Mcp Tool 어노테이션 대신, low-level api를 통해 직접 툴 등록

--- backend/src/main/java/com/qlmcp/backend/config/ToolConfig.java
+++ backend/src/main/java/com/qlmcp/backend/config/ToolConfig.java
@@ -1,19 +1,38 @@
 package com.qlmcp.backend.config;
 
-import com.qlmcp.backend.tool.MemoryTool;
+import java.util.Arrays;
+import java.util.List;
 
+import com.qlmcp.backend.tool.MemoryTool;
+import com.qlmcp.backend.tool.QueryTool;
+
+import org.springframework.ai.mcp.McpToolUtils;
 import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.MimeType;
+
+import io.modelcontextprotocol.server.McpStatelessServerFeatures.SyncToolSpecification;
 
 @Configuration
 public class ToolConfig {
 
     @Bean
-    public ToolCallbackProvider toolCallbackProvider(MemoryTool memoryTool) {
+    ToolCallbackProvider toolCallbackProvider(MemoryTool memoryTool) {
         return MethodToolCallbackProvider.builder()
                 .toolObjects(memoryTool)
                 .build();
+    }
+
+    @Bean
+    List<SyncToolSpecification> externalTool(QueryTool queryTool) {
+        ToolCallbackProvider toolCallbackProvider = MethodToolCallbackProvider.builder()
+                .toolObjects(queryTool)
+                .build();
+        return Arrays.stream(toolCallbackProvider.getToolCallbacks())
+                .map(toolCallback -> McpToolUtils.toStatelessSyncToolSpecification(toolCallback,
+                        MimeType.valueOf("application/json")))
+                .toList();
     }
 }
