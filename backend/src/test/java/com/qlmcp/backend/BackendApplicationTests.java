6c1c9b0040d8c3fca992898dbe8014909f329f6e
Lee Hyungi <geist9110@naver.com>
2025-12-08 15:07:35 +0900

    chore: 테스트 display name 변경

--- backend/src/test/java/com/qlmcp/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/qlmcp/backend/BackendApplicationTests.java
@@ -29,15 +29,15 @@ class BackendApplicationTests {
     private Environment environment;
 
     @Test
-    @DisplayName("Spring Application Context가 정상적으로 로드되는지 테스트")
-    void contextLoads() {
+    @DisplayName("[SUCCESS] contextLoads - no input -> load spring application context")
+    void contextLoads_NoInput_LoadSpringApplicationContext() {
         String appName = environment.getProperty("spring.application.name");
         assertEquals("backend", appName);
     }
 
     @Test
-    @DisplayName("SyncTool 전체를 불렀을 때 서버 툴만 불러오는지 테스트")
-    void internalToolTest() {
+    @DisplayName("[SUCCESS] externalTool - no input -> load only external tool")
+    void externalTool_NoInput_LoadOnlyExternalTool() {
         List<SyncToolSpecification> serverTools = tools.stream().flatMap(List::stream).toList();
         assertAll(() -> assertNotNull(serverTools), () -> assertEquals(1, serverTools.size()));
     }
